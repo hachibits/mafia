aaa3120e86aab88d543573fe19e90dc68bc9fee9
Oliver Xu <olxu@hotmail.com>
2021-08-28 23:51:53 +1000

    implementing the logic that lets players take actions

--- game.ts
+++ game.ts
@@ -1,23 +1,112 @@
-interface GameState {
+export interface GameState {
     players: Player[];
     dayIndex: number;
+    //phase: boolean;
+    proposedAction: SomeAction | null;
 }
 
-interface Player {
+export interface Player {
     name: string;
-    role: ClassType[];
+    role: ClassType;
 }
 
-class Game {
+export type SomeAction = VoteAction | KillAction | PassAction;
+
+export class Game {
     private state: GameState;
 
-    public acceptAction(action: Action): void {}
+    constructor() {
+        this.state = Game.setup();
+    }
 
+    public getStateSnapshot(): GameState {
+        return Game.cloneState(this.state);
+    }
+
+    public acceptAction(action: SomeAction): GameState {
+        this.state = this.executeAction(this.state, action);
+
+        return this.state;
+    }
+
+    public getAvailableActions() {
+        return Game.getAvailableActions(this.state);
+    }
+
+    private static getAvailableActions(state: GameState): SomeAction[] {
+        const currentPlayer = state.players[state.dayIndex];
+        const otherPlayers = state.players.filter(p => p !== currentPlayer);
+
+        let availableActions = [];
+
+        for (const player of state.players) {
+            availableActions = availableActions.concat(
+                Game.getActionsForPlayer(
+                    player,
+                    otherPlayers,
+                    player === currentPlayer,
+                    state.proposedAction
+                )
+            );
+        }
+
+        return availableActions;
+    }
+
+    private static getActionsForPlayer(
+        player: Player,
+        otherPlayers: Player[],
+        isTheirTurn: boolean,
+        proposedAction: SomeAction
+    ): SomeAction[] {
+        let result: SomeAction[] = [];
+
+        if (isTheirTurn) {
+            let roleActions: SomeAction[] = [];
+
+            roleActions = roleActions.concat(
+                Game.getActionsForType(player.role, player, proposedAction, otherPlayers)
+            );    
+
+            result = result.concat(roleActions);
+            result = result.concat(
+                [
+                    //new VoteAction(player, target),
+                    new PassAction(player)
+                ]
+            );
+
+        } else {
+        }
+
+        return [...result];
+    }
+
+    private static getActionsForType(
+        type: ClassType,
+        origin: Player,
+        proposedAction: SomeAction,
+        otherPlayers: Player[],
+    ): SomeAction[] {
+        let playerActions = [];
+
+        switch () {
+
+        }
+
+        return playerActions;
+    }
+
+    private static cloneState(state: GameState): GameState {
+        return JSON.parse(JSON.stringify(state));
+    }
     private executeAction(
         state: GameState,
-        action: KillAction | PassAction
+        action: SomeAction
     ): GameState {
         switch (action.type) {
+            case ActionType.Vote:
+                return Game.executeVote(action, state);
             case ActionType.Kill:
                 return Game.executeKill(action, state);
             case ActionType.Pass:
@@ -45,11 +134,14 @@ class Game {
     private static setup(): GameState {
         return {
             dayIndex: 0,
+            proposedAction: null,
             players: []
         };
     }
 
 }
+
+
 
 class Action {
     origin: Player;
@@ -59,7 +151,9 @@ class Action {
     }
 }
 
-class VoteAction extends Action {}
+class VoteAction extends Action {
+    // conditional: at least 2 votes to execute action
+}
 
 class KillAction extends Action {
     type: ActionType.Kill = ActionType.Kill;
@@ -81,7 +175,6 @@ class PassAction extends Action {
         //this.origin.togglePass();
     }
 }
-
 
 enum ClassType {
     Detective,
